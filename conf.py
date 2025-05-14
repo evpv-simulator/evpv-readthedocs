e2d9318fd27fd4a1d35de04e9a1a084a5bc94be8
Jérémy Dumoulin <jeremy.dumoulin@epfl.ch>
2025-05-14 17:04:57 +0200

    Modify template

--- conf.py
+++ conf.py
@@ -42,6 +42,9 @@ html_logo = "img/logo_small.png"
 
 html_theme = "sphinx_rtd_theme"
 html_static_path = ['_static']
+html_css_files = [
+    'custom.css',
+]
 html_show_sourcelink = False
 
 html_context = {}

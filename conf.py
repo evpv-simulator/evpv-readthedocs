da2372a5e221bb1798ecad37b123b6eb410f05a3
Jérémy Dumoulin <jeremy.dumoulin@epfl.ch>
2025-05-27 15:58:35 +0200

    Apdate API

--- conf.py
+++ conf.py
@@ -10,7 +10,7 @@ import sys
 import os
 import sphinx_rtd_theme
 import evpv.vehicle
-#sys.path.insert(0, os.path.abspath('../'))  # Adjust as needed to find evpv/
+sys.path.insert(0, os.path.abspath('evpv/'))  # Adjust as needed to find evpv/
 
 project = 'EVPV'
 copyright = '2024, Jeremy Dumoulin'
